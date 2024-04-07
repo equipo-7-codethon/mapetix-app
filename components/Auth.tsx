0447dbc79929b04b457175de2e487287fdb523eb
RedkarRiv <128104355+RedkarRiv@users.noreply.github.com>
2024-04-07 17:03:52 +0200

    Deleted folder Backend and moved components to root project

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -8,7 +8,7 @@ import { Button, Input } from 'react-native-elements'
 // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
 // if the user's session is terminated. This should only be registered once.
 AppState.addEventListener('change', (state) => {
-  if (state === 'active') { 
+  if (state === 'active') {
     supabase.auth.startAutoRefresh()
   } else {
     supabase.auth.stopAutoRefresh()
