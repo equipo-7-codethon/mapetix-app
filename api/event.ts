8449e39d5d6c8a050bcf0938c244f6f260fc48ba
NotJaviX <javierlloret2011@gmail.com>
2025-02-23 10:56:32 +0100

    filtros de eventos y valoración

--- api/event.ts
+++ api/event.ts
@@ -6,11 +6,46 @@ const eventApi = createApi({
   baseQuery: authApiQuery,
   endpoints: (builder) => ({
     getEvents: builder.query({
-      query: () => 'allevents',
+      query: ({ price, valoration, category }) => {
+        const params = new URLSearchParams();
+
+        if (price != null && price !== undefined) {
+          params.append('price', price.toString());
+        }
+        
+        if (valoration != null && valoration !== undefined) {
+            params.append('valoration', valoration.toString());
+        }
+        
+        if (category != null && category !== undefined) {
+            params.append('category', category.toString());
+        }
+          return `allevents?${params.toString()}`;
+      },
     }),
     getEventDetail: builder.query({
       query: (id) => `event/${id}`,
       transformResponse: (response) => response[0],
+    }),
+    getEventUserRate: builder.query({
+      query: (id) => `event/rate/${id}`,
+      transformResponse: (response) => response[0],
+    }),
+    rateEvent: builder.mutation({
+      query: ({ eventId, nota, description }) => ({
+        url: `plan/rate/${eventId}`,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',  // Esto es importante
+        },
+        body: JSON.stringify({
+          Nota: nota,
+          Description: description,
+        }),
+      }),
+    }),
+    getCategories: builder.query({
+      query: () => 'event/categories',
     }),
   }),
 });
@@ -18,6 +53,9 @@ const eventApi = createApi({
 export const {
   useGetEventsQuery,
   useGetEventDetailQuery,
+  useGetEventUserRateQuery,
   useLazyGetEventsQuery,
+  useRateEventMutation,
+  useLazyGetCategoriesQuery
 } = eventApi;
 export default eventApi;
